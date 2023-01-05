29c6a567c05a80a236ab29990255cf915750b0ce
larswie <118259123+larswie@users.noreply.github.com>
2023-01-05 18:46:11 +0100

    Add files via upload

---
+++ aip-charts.py
@@ -0,0 +1,69 @@
+import base64
+from bs4 import BeautifulSoup
+import requests
+import os
+import platform
+
+def create_dir(ad_url):
+    html_text = requests.get(ad_url).text
+    path = os.getcwd()
+    if platform.system() == 'Windows':
+        slash='\\'
+    else:
+        slash='/'
+
+    soup = BeautifulSoup(html_text, 'html.parser')    
+    icao = soup.find('div', class_='headlineText left').get_text().replace(" ", "_").rsplit('_', 1)[-1]
+    newdir = path+slash+icao
+    print(f'--------------------------------------------- {icao} ---------------------------------------------')
+    print(f'Creating Directory {newdir} if necessary')
+    os.makedirs(newdir, exist_ok=True)
+    icao_dir = newdir+slash
+
+    return(icao_dir)
+
+def create_png(aip_url, icao_dir):
+    html_text = requests.get(aip_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    filename = soup.find('div', class_='headlineText left').get_text().replace(" ", "_")
+    print(f'Downloading Chart {filename}')
+
+    link = soup.find('img', class_='pageImage')['src']
+
+    linksplit = link.split(',')
+    b64img = str.encode(linksplit[1])
+
+    print(f'Saving Chart as {icao_dir}{filename}.png')
+
+    with open(f"{icao_dir}{filename}.png", "wb") as fh:
+        fh.write(base64.decodebytes(b64img))
+
+url = 'https://aip.dfs.de/basicVFR/2022DEC15/f8b39000e696651bdd6398c1bb5828a2.html'
+
+html_text = requests.get(url).text
+
+soup = BeautifulSoup(html_text, 'html.parser')
+
+for links in soup.find_all('a', class_='folder-link', href=True):
+    if links.text[0:3].strip() != 'AD':
+        # print(f'Found the URL:, {links["href"]}')
+        letter_url = f'https://aip.dfs.de/basicVFR/2022DEC15/{links["href"]}'
+        letter_url_text = requests.get(letter_url).text
+        
+        letter_soup = BeautifulSoup(letter_url_text, 'html.parser')
+
+        for ad_links in letter_soup.find_all('a', class_='folder-link', href=True):
+            # print(f'https://aip.dfs.de/basicVFR/2022DEC15/{ad_links["href"]}')
+            ad_url = f'https://aip.dfs.de/basicVFR/2022DEC15/{ad_links["href"]}'
+
+            icao_dir = create_dir(ad_url)
+
+            ad_url_text = requests.get(ad_url).text
+
+            ad_soup = BeautifulSoup(ad_url_text, 'html.parser')
+
+            for chart_links in ad_soup.find_all('a', class_='document-link', href=True):
+                # print(f'https://aip.dfs.de/basicVFR/2022DEC15/{chart_links["href"]}')
+                chart_url = f'https://aip.dfs.de/basicVFR/2022DEC15/{chart_links["href"]}'
+                create_png(chart_url, icao_dir)
